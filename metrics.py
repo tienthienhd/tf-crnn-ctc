0f46789d2cba660d476a0a523278abd141537565
thiennt <tienthienhd@gmail.com>
2021-06-12 14:16:43 +0700

    add metrics.py

--- metrics.py
+++ metrics.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 import config
 
@@ -20,6 +20,27 @@ def char_acc(y_true, y_pred):
         tf.reduce_sum(input_tensor=weights, axis=1))
     char_acc_batch = tf.reduce_mean(input_tensor=accuracy_per_example)
     return char_acc_batch
+
+#
+# print(char_acc(np.array([[3,6,1,1,7,2,9,8,7, -1 , -1, -1]]), np.load(open('test.npy', 'rb'))))
+
+def seq_acc(y_true, y_pred):
+    input_len = np.ones(y_pred.shape[0]) * y_pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search
+    results = tf.keras.backend.ctc_decode(y_pred, input_length=input_len, greedy=True)[0][0][
+              :, :config.DatasetConfig.max_len
+              ]
+    preds_idx = tf.cast(results, dtype=tf.int32)
+    targets = tf.cast(y_true, dtype=tf.int32)
+    const_rej_char = tf.constant(-1, shape=targets.get_shape())
+    weights = tf.cast(tf.not_equal(targets, const_rej_char), dtype=tf.float32)
+    correct_chars = tf.cast(tf.equal(preds_idx, targets), dtype=tf.float32)
+    accuracy_per_example = tf.compat.v1.div(
+        tf.reduce_sum(input_tensor=tf.multiply(correct_chars, weights), axis=1),
+        tf.reduce_sum(input_tensor=weights, axis=1))
+    char_acc_batch = tf.reduce_mean(input_tensor=accuracy_per_example)
+
+
 
 class CharacterAccuracy(tf.keras.metrics.Metric):
     def __init__(self, name='char_acc', **kwargs):
