bc766af2261ca04e0fff73679cd6ebfd07874b0d
tienthienhd <tienthienhd@gmail.com>
2023-05-18 22:32:47 +0700

    new captcha

--- build_image.sh
+++ build_image.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker build -t  docker-registry.tima-ai.dev/thiennt/captcha_service:1.0.8 .
-docker push  docker-registry.tima-ai.dev/thiennt/captcha_service:1.0.8
+#docker build -t  docker-registry.tima-ai.dev/thiennt/captcha_service:1.0.8 .
+#docker push  docker-registry.tima-ai.dev/thiennt/captcha_service:1.0.8
+
+docker build -t  tienthienhd/captcha_service:1.0.0 .
+docker push  tienthienhd/captcha_service:1
