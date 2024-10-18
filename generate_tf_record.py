66a17e158fe5c03e997658669197e02fad29f2a0
thiennt <tienthienhs@gmail.com>
2024-10-19 03:35:28 +0700

    data train

--- generate_tf_record.py
+++ generate_tf_record.py
@@ -215,17 +215,17 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default="gplx")
+    parser.add_argument('--data', type=str, default="evisa")
     parser.add_argument('--num', type=int, default="0")
     parser.add_argument('--filter_charset', type=bool, default="true")
-    parser.add_argument('--cfg', type=str, default="config.json")
+    parser.add_argument('--cfg', type=str, default="evisa.json")
 
     args = parser.parse_args()
 
-    if args.cfg != 'config.json':
+    if args.cfg != 'evisa.json':
         cfg = args.cfg
     else:
-        cfg = os.path.join(f'./datasets/{args.data}/models/config.json')
+        cfg = os.path.join(f'./datasets/{args.data}/models/evisa.json')
     num = args.num
     filter_charset = args.filter_charset
     config.load_config(cfg)
